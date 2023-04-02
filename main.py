00a6ec50e4a6b4a67f405bd2344c92b6c9b86f9d
unknown <ff245185@gmail.com>
2023-04-02 14:20:30 +0600

    error fix

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 print("plus +")
-print("minas -")
-print("multipal *")
+print("minus -")
+print("multiple *")
 print("divide /")
+
+a= 56
+b= 56
+c= 87
 
 number1 = int(input("enter fast number :"))
 number2 = int(input("enter secend number :"))
-opt = input("select anyone...(+,-,*,/) :")
+opt = input("select anyone...(+,-,*,/,^) :")
 
 if opt == "+":
   print(number1 + number2)
@@ -18,6 +22,9 @@ elif opt == "*":
 
 elif opt == "/":
   print(number1 / number2)
+
+elif opt == "^":
+  print(a + b * c / a + b)
 
 else:
   print("invlide number")
